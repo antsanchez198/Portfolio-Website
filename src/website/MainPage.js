51b07776b72b3960140cb93a34c4b1a88331ed7d
Anthony Sanchez <antsanchez198@gmail.com>
2023-02-14 23:47:35 -0500

    adding more information

--- src/website/MainPage.js
+++ src/website/MainPage.js
@@ -5,16 +5,29 @@ import photo from '../'
 export default function MainPage() {
     return (
         <>
-        <NavBar/>
-        <div className = "body">
-            <h1>Anthony Sanchez</h1>
-            <img className = "img" src="/myheadshot.png"></img>
-            
-       
-        <p className='paragraph'>A hands on, detail orientated, and professional Web Developer with entry-level experience in HTML, CSS and React. Who’s
-motivated to contribute to large scale projects with a proven track record of developing polished and creative user interfaces.</p>
-        </div>
-        <h1>Experience</h1> 
+            <NavBar />
+            <div className="body">
+                <h1>Anthony Sanchez</h1>
+                <img className="img" src="/myheadshot.png"></img>
+
+
+                <p className='paragraph'>A hands on, detail orientated, and professional Web Developer with entry-level experience in HTML, CSS and React. Who’s
+                    motivated to contribute to large scale projects with a proven track record of developing polished and creative user interfaces.</p>
+            </div>
+            <h1>Experience</h1>
+            <h2>Knomadix</h2>
+            <h3>Front End Developer Intern</h3>
+            <p>Designed and built new responsive layouts and features for an educational based startup company using React.js, HTML,
+                and CSS. Resulting in an improvement of the readability of information and overall front-end design</p>
+            <p>Collaborated closely with Backend developers to implement dozens of APIs using React Redux and debugging any issues
+                during the process</p>
+            <p>Handled JSON Data received from the API containing assignment and subscription details to display on tables, graphs, and
+                dashboards with the use of Material UI components</p>
+            <p>Responsible for setting up forms using JavaScript, which stored data from the user interface to be passed to the API for a
+                successful update of subscription plans, notifications, and school district information</p>
+            <h1>Projects</h1>
+            <h1>Skills</h1>
         </>
+
     )
 }
