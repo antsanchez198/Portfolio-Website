c856df227679b4146f12902900398fee05664802
Anthony Sanchez <antsanchez198@gmail.com>
2023-02-16 23:49:27 -0500

    added grid

---
+++ src/website/Card.js
@@ -0,0 +1,10 @@
+import './MainPage.css';
+
+export default function Card() {
+    return (
+        <div className="card">
+            <h2>Title</h2>
+            <img src="https://cdn.theatlantic.com/media/mt/science/cat_caviar.jpg"></img>
+        </div>
+    )
+}
--- src/website/MainPage.js
+++ src/website/MainPage.js
@@ -1,18 +1,18 @@
 import './MainPage.css';
 import NavBar from './NavBar';
-import photo from '../'
+import Card from './Card';
 
 export default function MainPage() {
     return (
         <>
             <NavBar />
-            <h1>Anthony Sanchez</h1>
+            <h1 className="margin">Anthony Sanchez</h1>
             <div className="container">
                 <img className="img" src="/myheadshot.png"></img>
                 <p className='paragraph'>A hands on, detail orientated, and professional Web Developer with entry-level experience in HTML, CSS and React. Who’s
                     motivated to contribute to large scale projects with a proven track record of developing polished and creative user interfaces.</p>
             </div>
-            <div className='exp-container'>
+            <div className='exp-container margin'>
                 <div className='left'><h1>Professional Experience</h1></div>
                 <div className='line'></div>
                 <div className='right'>
@@ -29,17 +29,20 @@ export default function MainPage() {
                             </ul>
                         </div>
                     </div>
-                    <p>Designed and built new responsive layouts and features for an educational based startup company using React.js, HTML,
-                        and CSS. Resulting in an improvement of the readability of information and overall front-end design</p>
-                    <p>Collaborated closely with Backend developers to implement dozens of APIs using React Redux and debugging any issues
-                        during the process</p>
-                    <p>Handled JSON Data received from the API containing assignment and subscription details to display on tables, graphs, and
-                        dashboards with the use of Material UI components</p>
-                    <p>Responsible for setting up forms using JavaScript, which stored data from the user interface to be passed to the API for a
-                        successful update of subscription plans, notifications, and school district information</p>
+                    <p>Designed and built new responsive layouts and features for an educational based startup company named Knomadix using React.js, HTML,
+                        and CSS. Who's mission is to transform how students learn using Artificial Intelligence to leverage insights from assignments to help create personalized lesson plans.</p>
                 </div>
             </div>
-            <h1>Projects</h1>
+            <div className="project-section">
+                <h1 className="margin">Projects</h1>
+                <div className="cards-container">
+                    <Card/>
+                    <Card/>
+                    <Card/>
+                    <Card/>
+                    <Card/>
+                </div>
+            </div>
             <h1>Skills</h1>
         </>
 
